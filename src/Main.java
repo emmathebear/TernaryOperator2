49cc57c0f1d005fe42ef5bcb7aa4db869c1b5b01
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-03-18 15:32:18 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkString("Questa è una stringa"));
+        System.out.println(checkString("Questa "));
+    }
+
+    private static String checkString(String value) {
+        return value.length()>=10?"Lunghezza maggiore o uguale di 10":"Lunghezza minore di 10" ;
+    }
+}
